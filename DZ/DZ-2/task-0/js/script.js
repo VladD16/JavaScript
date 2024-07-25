bd8a081fdae575b0002e62696c407364ececab45
VladD16 <162788325+VladD16@users.noreply.github.com>
2024-07-25 11:26:34 +0300

    Add files via upload

--- DZ/DZ-2/task-0/js/script.js
+++ DZ/DZ-2/task-0/js/script.js
@@ -2,3 +2,18 @@
 
 let a = parseFloat(prompt("Введіть значення перемінної 'a'", "1"))
 let b = parseFloat(prompt("Введіть значення  перемінної 'b'", "1"))
+let c = parseFloat(prompt("Введіть значення  перемінної 'c'", "1"))
+
+const s1 = a + 12 + b
+const s2 = Math.sqrt((a + b) / (2 * a))
+const s3 = Math.cbrt((a + b) * c)
+const s4 = Math.sin(a / -b)
+
+document.write(`<p> a = ${a}, b = ${b}, c = ${c} </p>`)
+
+document.write(`<p>
+                S1= ${s1} <br>
+                S2= ${s2} <br>
+                S3= ${s3} <br>
+                S4= ${s4}
+                </p>`)
